0f4e77fa27d614abe027fe0e4da2785b594c661b
root <root@instance-20240803-123435.me-central1-c.c.sacred-reality-422708-h4.internal>
2024-08-19 15:05:36 +0000

    add animation

--- public/game.js
+++ public/game.js
@@ -8,11 +8,14 @@ kaboom({
     height: HEIGHT,
     background: [255, 255, 255]
 })
-
 // Load graphics for player and ball
 loadRoot("https://i.ibb.co/")
-loadSprite("doge", "8BbNfDk/image.png")
-loadSprite("chuck", "w4JcHfs/image.png")
+loadSprite("doge", "cgcb8bB/image.png")
+loadSprite("doge3","vcxZQGD/image.png")
+loadSprite("doge2", "9NGY7Dk/image.png")
+loadSprite("chuck", "0JXjGmX/image.png")
+loadSprite("chuck2", "1KxcCJw/image.png")
+loadSprite("chuck3", "LnXM1Rv/image.png")
 loadSprite("ball", "sqC2Gth/image.png")
 loadFont("Teko", "5cnqmZ8/Teko.png", 28, 50) // Load the "Teko" font
 
@@ -23,7 +26,7 @@ const players = [
         area(),
         solid(),
         origin("center"),
-        pos(65, HEIGHT / 2), // Left
+        pos(85, HEIGHT / 2), // Left
         { score: 0 },
         "player"
     ]),
@@ -61,7 +64,7 @@ const ball = add([
     origin("center"),
     pos(WIDTH / 2, HEIGHT / 2), // Middle of screen
     { // Horizontal and vertical speeds
-        speed_x: -2400,
+        speed_x: -5000,
         speed_y: 0
     }
 ])
@@ -73,7 +76,18 @@ loop(0.1, function () {
         ball.speed_y = -ball.speed_y
     }
 })
-
+var first = true;
+setInterval(function(){
+    if(first == true ){
+        players[0].use(sprite("doge3"));
+        players[1].use(sprite("chuck3"));
+        first = false;
+    }else{
+        players[0].use(sprite("doge"));
+        players[1].use(sprite("chuck"));
+        first = true;
+    }
+},300);
 // Number of player being controlled by arrow keys
 var playerSelected = 0
 
@@ -85,8 +99,23 @@ ball.onCollide("player", function () {
     if (negative == 1) {
         ball.speed_y = -ball.speed_y
     }
+    if(playerSelected === 1){
+        players[1].use(sprite("chuck2"))
+        setTimeout(function(){
+            players[1].use(sprite("chuck"))
+            playerSelected = 1 - playerSelected // Switch player selected
 
-    playerSelected = 1 - playerSelected // Switch player selected
+        },100);
+
+    }else if(playerSelected === 0){
+        players[0].use(sprite("doge2"))
+        setTimeout(function(){
+            players[0].use(sprite("doge"))
+            playerSelected = 1 - playerSelected // Switch player selected
+
+        },100);
+    }
+
 })
 
 // Check if ball goes off screen horizontally
@@ -94,7 +123,7 @@ ball.onUpdate(function () {
     // X coordinate less than 8 or more than WIDTH subtract 8, meaning player has failed to catch it
     if (ball.pos.x < 8 || ball.pos.x > WIDTH-8) {
         // Reset ball to centre of screen
-        ball.speed_x = -2400
+        ball.speed_x = -5000
         ball.speed_y = 0
         ball.moveTo(WIDTH / 2, HEIGHT / 2)
 
@@ -103,19 +132,30 @@ ball.onUpdate(function () {
         scoreboard.text = `${players[0].score}:${players[1].score}`
         playerSelected = 0
     }
+    
+
 })
 
 // Up key pressed
 onKeyDown('up', function () {
-    players[playerSelected].move(0, -PLAYER_SPEED) // Move vertically up
+    players[0].move(0, -PLAYER_SPEED) // Move vertically up
 })
 
 // Down key pressed
 onKeyDown('down', function () {
-    players[playerSelected].move(0, PLAYER_SPEED) // Move vertically down
+    players[0].move(0, PLAYER_SPEED) // Move vertically down
 })
 
+onKeyDown('z', function () {
+    players[1].move(0, -PLAYER_SPEED) // Move vertically up
+
+})
+onKeyDown('s', function () {
+    players[1].move(0, PLAYER_SPEED) // Move vertically down
+
+})
 // Q key pressed
 onKeyPress('q', function () {
     debug.paused = !debug.paused // Pause/unpause game
 })
+
